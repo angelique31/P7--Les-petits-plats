cc7dddcc298d9c0cea8fa8b6724ba6d6de70c6f9
angelique_rosin <angie1979.capucine@yahoo.fr>
2022-12-29 21:19:32 +0100

    programmation de la barre de recherche

--- src/components/search/search.js
+++ src/components/search/search.js
@@ -49,35 +49,3 @@ function search (recipes) {
 export default search;
 
 
-/**
- * Fonction de recherche avec la boucle for
- */
-// function search(recipes) {
-//   // console.log(recipes);
-//   let recipesArray = [];
-//   const filterData = (e) => {
-//     const inputLetters = formatText(e.target.value);
-//     if (inputLetters.length > 2) {
-//       for (let i = 0; i < recipes.length; i++) {
-//         // console.log(recipes[i])
-//         if (formatText(recipes[i].name).includes(inputLetters) ||
-//             formatText(recipes[i].description).includes(inputLetters)) {
-//                 recipesArray.push(recipes[i]);
-//         } else {
-//           for (let j = 0; j < recipes[i].ingredients.length; j++) {
-//             if (formatText(recipes[i].ingredients[j].ingredient).includes(inputLetters)) {
-//                 recipesArray.push(recipes[i]);
-//             }
-//           }
-//         }
-//         console.log(recipesArray);
-//       }
-
-//       return recipesArray;
-//     }
-//     displayRecipes(recipesArray); //fonction appelée qui permet le tri des cartes des recettes
-//     filtersList(recipesArray);
-//   };
-//   searchInput.addEventListener("input", filterData);
-// }
-// export default search;
